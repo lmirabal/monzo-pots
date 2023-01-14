49149f1663dd0067a76113cee8cc65be07986632
Luis Mirabal <lmirabal@gmail.com>
2023-01-14 08:16:42 +0000

    Initial project as generated by intellij

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "lmirabal"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
