1c613de6693cc9f82f98039651ae441bac6fec50
Luis Mirabal <lmirabal@gmail.com>
2023-02-05 20:20:11 +0000

    Refactor fund distribution tests
    Tests were getting quite long, so extract a few extension functions to
    hide some of the implementation details from the test cases. This should
    make them a lot easier to follow.

--- src/test/kotlin/FundsDistributionTest.kt
+++ src/test/kotlin/FundsDistributionTest.kt
@@ -1,7 +1,10 @@
 package lmirabal
 
+import lmirabal.finance.Amount
 import lmirabal.finance.pounds
+import lmirabal.model.Account
 import lmirabal.model.AccountAddress
+import lmirabal.model.Pot
 import lmirabal.model.PotName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -11,18 +14,15 @@ class FundsDistributionTest {
 
     private val bank = StubBank()
     private val accountAddress = AccountAddress("123456", "123456789")
+    private val account = bank.createAccount(CreateAccountRequest(accountAddress, 0.pounds))
 
     @Test
     fun `distribute funds`() {
-        val account = bank.createAccount(CreateAccountRequest(accountAddress, 0.pounds))
-        val source = PotName("source")
-        val destination1 = PotName("destination 1")
-        val destination2 = PotName("destination 2")
-        val remainder = PotName("remainder")
-        bank.createPot(CreatePotRequest(account, source, 300.pounds))
-        bank.createPot(CreatePotRequest(account, destination1, 0.pounds))
-        bank.createPot(CreatePotRequest(account, destination2, 0.pounds))
-        bank.createPot(CreatePotRequest(account, remainder, 0.pounds))
+        val source = bank.createPot("source", 300)
+        val destination1 = bank.createPot("destination 1", 0)
+        val destination2 = bank.createPot("destination 2", 0)
+        val remainder = bank.createPot("remainder", 0)
+
         val manifest = DistributionManifest(
             mainAccount = accountAddress,
             source = source,
@@ -33,26 +33,22 @@ class FundsDistributionTest {
             keepInMainAccount = 30.pounds,
             remainder = Remainder(to = remainder, atLeast = 10.pounds)
         )
-
         distributeFunds(bank, manifest)
 
-        val pots = bank.getPotsFor(account)
-        assertEquals(0.pounds, pots[source].balance)
-        assertEquals(30.pounds, bank.getAccountBy(accountAddress).balance)
-        assertEquals(50.pounds, pots[destination1].balance)
-        assertEquals(70.pounds, pots[destination2].balance)
-        assertEquals(150.pounds, pots[remainder].balance)
+        bank.asserting()
+            .potBalance(source, 0)
+            .accountBalance(30)
+            .potBalance(destination1, 50)
+            .potBalance(destination2, 70)
+            .potBalance(remainder, 150)
     }
 
     @Test
     fun `distribute just enough funds`() {
-        val account = bank.createAccount(CreateAccountRequest(accountAddress, 0.pounds))
-        val source = PotName("source")
-        val destination = PotName("destination")
-        val remainder = PotName("remainder")
-        bank.createPot(CreatePotRequest(account, source, 90.pounds))
-        bank.createPot(CreatePotRequest(account, destination, 0.pounds))
-        bank.createPot(CreatePotRequest(account, remainder, 0.pounds))
+        val source = bank.createPot("source", 90)
+        val destination = bank.createPot("destination", 0)
+        val remainder = bank.createPot("remainder", 0)
+
         val manifest = DistributionManifest(
             mainAccount = accountAddress,
             source = source,
@@ -60,25 +56,21 @@ class FundsDistributionTest {
             keepInMainAccount = 30.pounds,
             remainder = Remainder(to = remainder, atLeast = 10.pounds)
         )
-
         distributeFunds(bank, manifest)
 
-        val pots = bank.getPotsFor(account)
-        assertEquals(0.pounds, pots[source].balance)
-        assertEquals(30.pounds, bank.getAccountBy(accountAddress).balance)
-        assertEquals(50.pounds, pots[destination].balance)
-        assertEquals(10.pounds, pots[remainder].balance)
+        bank.asserting()
+            .potBalance(source, 0)
+            .accountBalance(30)
+            .potBalance(destination, 50)
+            .potBalance(remainder, 10)
     }
 
     @Test
-    fun `not enough funds to distribute`() {
-        val account = bank.createAccount(CreateAccountRequest(accountAddress, 0.pounds))
-        val source = PotName("source")
-        val destination = PotName("destination")
-        val remainder = PotName("remainder")
-        bank.createPot(CreatePotRequest(account, source, 100.pounds))
-        bank.createPot(CreatePotRequest(account, destination, 0.pounds))
-        bank.createPot(CreatePotRequest(account, remainder, 0.pounds))
+    fun `does not distribute when not enough funds`() {
+        val source = bank.createPot("source", 100)
+        val destination = bank.createPot("destination", 0)
+        val remainder = bank.createPot("remainder", 0)
+
         val manifest = DistributionManifest(
             mainAccount = accountAddress,
             source = source,
@@ -86,10 +78,30 @@ class FundsDistributionTest {
             keepInMainAccount = 30.pounds,
             remainder = Remainder(to = remainder, atLeast = 10.pounds)
         )
-
         val exception = assertThrows<IllegalArgumentException> {
             distributeFunds(bank, manifest)
         }
+
         assertEquals("Not enough funds: required=£110.00 available=£100.00", exception.message)
+    }
+
+    private fun StubBank.createPot(name: String, initialBalance: Int) =
+        PotName(name)
+            .also { potName ->
+                createPot(CreatePotRequest(account, potName, Amount.ofPounds(initialBalance)))
+            }
+
+    private fun StubBank.asserting() = Asserter(getAccountBy(accountAddress), getPotsFor(account))
+
+    private class Asserter(private val account: Account, private val pots: List<Pot>) {
+        fun accountBalance(expectedBalance: Int): Asserter {
+            assertEquals(Amount.ofPounds(expectedBalance), account.balance)
+            return this
+        }
+
+        fun potBalance(potName: PotName, expectedBalance: Int): Asserter {
+            assertEquals(Amount.ofPounds(expectedBalance), pots[potName].balance)
+            return this
+        }
     }
 }
